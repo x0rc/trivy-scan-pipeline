d5a9309dfa8f331fa7f05d5c9f1578a6d6ea11f1
jananathbanuka <jananath.banuka@e-bot7.com>
2022-06-11 04:05:19 +0530

    critical

--- scripts/pull-request.sh
+++ scripts/pull-request.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo -e "---\n### $test_result_emoji Test Results  \n :whale: - ${IMAGE}  \n| Severity  | Number of Vulnerabilities|\n| ------------- | ------------- |\n| $critical_emoji High    | $critical2  |\n| $high_emoji High        | $high2  |\n" >> pr_body
+echo -e "---\n### $test_result_emoji Test Results  \n :whale: - ${IMAGE}  \n| Severity  | Number of Vulnerabilities|\n| ------------- | ------------- |\n| $critical_emoji Critical    | $critical2  |\n| $high_emoji High        | $high2  |\n" >> pr_body
 
 echo  ":memo: [View the Full Report](${GITHUB_ACTION_URL})" >> pr_body
 
